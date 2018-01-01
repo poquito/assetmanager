31472782812e2d4ad771c00b9358859a1fb75db7
mrodler <mrodler@users.noreply.github.com>
2018-01-01 13:27:43 +0100

    admin service hat nicht festgelegt, dass xml geliefert werden soll. korrigiert

--- at.poquito.assetmanager-impl/src/at/poquito/assetmanager/rest/RestfulRepositoryAdmin.java
+++ at.poquito.assetmanager-impl/src/at/poquito/assetmanager/rest/RestfulRepositoryAdmin.java
@@ -6,7 +6,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
 
 import at.poquito.assetmanager.AssetRepository;
@@ -30,6 +32,7 @@ public class RestfulRepositoryAdmin {
 
 	@GET
 	@Path("repositories")
+	@Produces(MediaType.APPLICATION_XML)
 	public AssetRepositoryIndex getRepositoryIndex() {
 		AssetManagerConfiguration configuration = app.loadConfiguration();
 		AssetRepositoryIndex index = new AssetRepositoryIndex(configuration.getRepositories());
